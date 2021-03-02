8c82b10c901018152be3f63d5dab7d648760b34b
Shivaji Nayan <shivaji.nay@2c2pexternal.com>
2021-03-02 20:20:29 +0530

    initial solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,28 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuotes = async(urls) => {
+
+    // create the promise
+    const promises = urls.map(async(url) => {
+
+        // execute mock of httpGet and map the error
+        const { status, body } = await httpGet(url).then(error => error);
+
+        // body parsing and get the message property
+        const { message } = JSON.parse(body);
+
+        // based on the status code, return the message
+        if (status === 200) {
+            return { 'Arnie Quote': message };
+        } else {
+            return { 'FAILURE': message };
+        }
+    });
+
+    // Return all promises
+    return Promise.all(promises);
 };
 
 module.exports = {
-  getArnieQuotes,
+    getArnieQuotes,
 };
--- src/get-arnie-quotes.test.js
+++ src/get-arnie-quotes.test.js
@@ -1,37 +1,38 @@
 const { getArnieQuotes } = require('./get-arnie-quotes');
 
 const urls = [
-  'http://www.smokeballdev.com/arnie0',
-  'http://www.smokeballdev.com/arnie1',
-  'http://www.smokeballdev.com/arnie2',
-  'http://www.smokeballdev.com/arnie3',
+    'http://www.smokeballdev.com/arnie0',
+    'http://www.smokeballdev.com/arnie1',
+    'http://www.smokeballdev.com/arnie2',
+    'http://www.smokeballdev.com/arnie3',
 ];
 
-test('expect no throws', () => {
-  expect.assertions(1);
-  expect(async () => await getArnieQuotes(urls)).not.toThrow(); 
-});
+describe("tests", function() {
+    it('expect no throws', () => {
+        expect.assertions(1);
+        expect(async() => await getArnieQuotes(urls)).not.toThrow();
+    });
 
-test('responses to be correct', async () => {
-  expect.assertions(5);
+    it('responses to be correct', async() => {
+        expect.assertions(5);
+        var results = await getArnieQuotes(urls);
 
-  const results = await getArnieQuotes(urls);
-  
-  expect(results.length).toBe(4);
+        expect(results.length).toBe(4);
 
-  expect(results[0]).toEqual({ 'Arnie Quote': 'Get to the chopper' });
-  expect(results[1]).toEqual({ 'Arnie Quote': 'MY NAME IS NOT QUAID' });
-  expect(results[2]).toEqual({ 'Arnie Quote': `What's wrong with Wolfie?` });
-  expect(results[3]).toEqual({ 'FAILURE': 'Your request has been terminated' });
-});
+        expect(results[0]).toEqual({ 'Arnie Quote': 'Get to the chopper' });
+        expect(results[1]).toEqual({ 'Arnie Quote': 'MY NAME IS NOT QUAID' });
+        expect(results[2]).toEqual({ 'Arnie Quote': `What's wrong with Wolfie?` });
+        expect(results[3]).toEqual({ 'FAILURE': 'Your request has been terminated' });
+    });
 
-test('code to be executed in less than 400ms', async () => {
-  expect.assertions(2);
+    it('code to be executed in less than 400ms', async() => {
+        expect.assertions(2);
 
-  const startTime = process.hrtime();
-  await getArnieQuotes(urls);
-  const [ seconds, nanos ] = process.hrtime(startTime);
-  
-  expect(seconds).toBe(0);
-  expect(nanos / 1000 / 1000).toBeLessThan(400);
-});
+        const startTime = process.hrtime();
+        await getArnieQuotes(urls);
+        const [seconds, nanos] = process.hrtime(startTime);
+
+        expect(seconds).toBe(0);
+        expect(nanos / 1000 / 1000).toBeLessThan(400);
+    });
+})
